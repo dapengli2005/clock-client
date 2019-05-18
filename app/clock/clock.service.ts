5cb644da333a815a20b76add4ea03ce730a42755
Dapeng Li <luislee818@gmail.com>
2019-05-18 16:34:04 +0200

    feature: show pagination in clock entries history

--- app/clock/clock.service.ts
+++ app/clock/clock.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Http, Request, Response, Headers, RequestOptions, RequestMethod } from '@angular/http';
 
 import { User } from '../models/user.interface';
 import { UserService } from '../user.service';
 import { API_BASE } from '../constants';
 
 import { ClockEntry } from './models/clock-entry.interface';
+import { PaginatedClockEntries, PaginationMeta } from './models/paginated-clock-entries.interface';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -14,6 +15,7 @@ import 'rxjs/add/observable/of';
 
 const NEXT_ENTRY_URL: string = `${API_BASE}/users/:id/clock_entries/next`;
 const REGISTER_ENTRY_URL: string = `${API_BASE}/users/:id/clock_entries`;
+const GET_ENTRIES_URL: string = `${API_BASE}/users/:id/clock_entries`;
 
 @Injectable()
 export class ClockService {
@@ -33,6 +35,29 @@ export class ClockService {
       .post(url, entry)
       .map((response: Response) => response.json())
       .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  getEntries(): Observable<PaginatedClockEntries> {
+    const url = GET_ENTRIES_URL.replace(':id', `${this.userService.getUser().id}`);
+    return this.http
+      .get(url)
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  getEntriesBy({ url, method }: PaginationMeta): Observable<PaginatedClockEntries> {
+    return this.http
+      .request(new Request({ url, method: this.mapMethod(method) }))
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  methodMapper = {
+    GET: RequestMethod.Get,
+    POST: RequestMethod.Post
+  };
+  private mapMethod(method: PaginationMeta['method']): RequestMethod {
+    return this.methodMapper[method];
   }
 }
 
--- app/clock/containers/clock-entry-history/clock-entry-history.component.ts
+++ app/clock/containers/clock-entry-history/clock-entry-history.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ClockService } from '../../clock.service';
+
+import { PaginatedClockEntries, PaginationMeta } from '../../models/paginated-clock-entries.interface';
+
 @Component({
   selector: 'clock-entry-history',
   template: `
-    <div>
-      Clock entry history.
+    <div *ngIf="paginatedEntries">
+      <div *ngFor="let entry of paginatedEntries.data">
+        {{entry.id}}
+      </div>
+      <a href="#" *ngIf="paginatedEntries.meta?.prev" (click)="goTo($event, paginatedEntries.meta.prev)">prev</a>
+      <a href="#" *ngIf="paginatedEntries.meta?.next" (click)="goTo($event, paginatedEntries.meta.next)">next</a>
     </div>
   `
 })
-export class ClockEntryHistoryComponent {
+export class ClockEntryHistoryComponent implements OnInit {
+  paginatedEntries: PaginatedClockEntries;
+
+  constructor(private clockService: ClockService) {
+  }
+
+  ngOnInit() {
+    this.clockService.getEntries()
+      .subscribe(val => this.paginatedEntries = val);
+  }
+
+  goTo(e, paginationMeta: PaginationMeta) {
+    e.preventDefault();
+    this.clockService.getEntriesBy(paginationMeta)
+      .subscribe(val => this.paginatedEntries = val);
+  }
 }
 
